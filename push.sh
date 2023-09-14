f2d14f4116d783d7316b7aef6c0babef9a920386
wenjing999 <2959440159@qq.com>
2023-09-14 10:59:15 +0800

    s

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global http.sslverify false
 git config --global user.name 'wenjing999'
 git config --global user.email '2959440159@qq.com'
 git add .
-git commit -m $1
+git commit -m 自动推送
 git push
